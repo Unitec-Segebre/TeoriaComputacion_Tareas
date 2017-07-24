6ca9a66e617ad39e66c896bd485b9857f9a45c2d
Juan Enrique Segebre <juansegebre@gmail.com>
2017-07-24 08:34:29 -0600

    array of class automata_node

--- automata.py
+++ automata.py
@@ -1,5 +1,5 @@
 import sys, pygame
-from node import node_state
+from automata_node import node_state
 pygame.init()
 
 size = width, height = 1024, 720
@@ -16,16 +16,16 @@ while 1:
 	if pygame.mouse.get_pressed()[0]:
 		flag_new_node = 1;
 		for node_to_move in nodes:
-			if node_to_move.collidepoint(pygame.mouse.get_pos()):
-				node_to_move.center = pygame.mouse.get_pos()
+			if node_to_move.rect.collidepoint(pygame.mouse.get_pos()):
+				node_to_move.rect.center = pygame.mouse.get_pos()
 				flag_new_node = 0;
 		if flag_new_node:
-			nodes.append(pygame.draw.circle(screen, (255, 255, 255), pygame.mouse.get_pos(), 50))
+			nodes.append(node_state(pygame.draw.circle(screen, (255, 255, 255), pygame.mouse.get_pos(), 50)))
 
 	if pygame.mouse.get_pressed()[2]:
 		print (len(nodes))
 
 	screen.fill(black)
 	for node_to_draw in nodes:
-		node_to_draw = pygame.draw.circle(screen, (255, 255, 255), node_to_draw.center, 50)
+		node_to_draw.rect = pygame.draw.circle(screen, (255, 255, 255), node_to_draw.rect.center, 50)
 	pygame.display.flip()
